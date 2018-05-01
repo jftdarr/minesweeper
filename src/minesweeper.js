4131df9c5b427fe7cda13a933549e19436cc0a0b
jtamagnadarr <jft.darr@gmail.com>
2018-04-30 22:32:55 -0400

    Step 11 accomplished

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,25 +1,16 @@
-//This code creates the framework for a minesweeper game.
-const printBoard = (board) => {
-  // Log 'Current Board'
-  // Log the first element of the board variable
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+// Run node minesweeper.js to run this code.
+
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+  let board = [];
+  for (r = 0; r <= numberOfRows; r++) {
+    let row = [];
+    for (col = 0; col <= numberOfColumns; col++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
-// Create a variable to store the board.
-const board = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
 
-printBoard(board);
-
-// Update row one, second element equal to 1
-board[0][1] = 1;
-
-// Update row one, second element equal to 1
-board[2][2] = 'B';
-
-printBoard(board);
+console.log(generatePlayerBoard(3,4));
+console.log(generatePlayerBoard(1,10));
